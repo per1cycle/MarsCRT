ef136f25856040114a9c94ca2e089468ce837278
per1cycle <pericycle.cc@gmail.com>
2024-09-23 12:33:14 +0800

    Upload Test(v0.0.1)

---
+++ tests/FinalTest.c
@@ -0,0 +1,9 @@
+// Final test v0.0.0
+#include "MarsCRT.h"
+
+int main()
+{
+    printf("Hello world");
+    printf("This is a printf test: %d, %s, %c\n", 1, "hello world\0", 'c');
+    return 0;
+}
